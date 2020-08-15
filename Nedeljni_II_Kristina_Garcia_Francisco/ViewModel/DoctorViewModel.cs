f1dd5dfd32172b4fa3857b94f7006b8b7909e27d
kris994 <kristipomo@gmail.com>
2020-08-15 15:03:22 +0200

    Added patient preview to doctor window

---
+++ Nedeljni_II_Kristina_Garcia_Francisco/ViewModel/DoctorViewModel.cs
@@ -0,0 +1,125 @@
+﻿using Nedeljni_II_Kristina_Garcia_Francisco.Commands;
+using Nedeljni_II_Kristina_Garcia_Francisco.DataAccess;
+using Nedeljni_II_Kristina_Garcia_Francisco.Model;
+using Nedeljni_II_Kristina_Garcia_Francisco.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Nedeljni_II_Kristina_Garcia_Francisco.ViewModel
+{
+    class DoctorViewModel : BaseViewModel
+    {
+        DoctorData docData = new DoctorData();
+        DoctorWindow docWindow;
+
+        #region Constructor
+        /// <summary>
+        /// Constructor with the doctor info window opening
+        /// </summary>
+        /// <param name="doctorWindowOpen">opends the doctor window</param>
+        public DoctorViewModel(DoctorWindow doctorWindowOpen)
+        {
+            docWindow = doctorWindowOpen;
+            DoctorPatientList = docData.GetAllPatientsFromDoctors(LoggedInUser.CurrentUser.UserID).ToList();
+        }
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// List of doctor
+        /// </summary>
+        private List<vwClinicPatient> doctorPatientList;
+        public List<vwClinicPatient> DoctorPatientList
+        {
+            get
+            {
+                return doctorPatientList;
+            }
+            set
+            {
+                doctorPatientList = value;
+                OnPropertyChanged("DoctorPatientList");
+            }
+        }
+
+        /// <summary>
+        /// Info label
+        /// </summary>
+        private string infoLabel;
+        public string InfoLabel
+        {
+            get
+            {
+                return infoLabel;
+            }
+            set
+            {
+                infoLabel = value;
+                OnPropertyChanged("InfoLabel");
+            }
+        }
+
+        /// <summary>
+        /// Info label background
+        /// </summary>
+        private string infoLabelBG;
+        public string InfoLabelBG
+        {
+            get
+            {
+                return infoLabelBG;
+            }
+            set
+            {
+                infoLabelBG = value;
+                OnPropertyChanged("InfoLabelBG");
+            }
+        }
+        #endregion
+
+        #region Commands      
+        /// <summary>
+        /// Command that logs off the user
+        /// </summary>
+        private ICommand logoff;
+        public ICommand Logoff
+        {
+            get
+            {
+                if (logoff == null)
+                {
+                    logoff = new RelayCommand(param => LogoffExecute(), param => CanLogoffExecute());
+                }
+                return logoff;
+            }
+        }
+
+        /// <summary>
+        /// Executes the logoff command
+        /// </summary>
+        private void LogoffExecute()
+        {
+            try
+            {
+                docWindow.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Checks if its possible to logoff
+        /// </summary>
+        /// <returns>true</returns>
+        private bool CanLogoffExecute()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
